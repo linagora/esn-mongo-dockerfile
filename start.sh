f4f59b905caf383585c40b1bc0ca119ff7ff692b
Stéphen Le Maistre <slemaistre@linagora.com>
2016-01-08 17:54:16 +0100

    Increase sleep time before initializing the replicaSet

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 [ -z "$REPLICATE_NAME" ] || replicate_name="$REPLICATE_NAME"
 [ -z "$OPLOG_SIZE" ]     || oplog_size="$OPLOG_SIZE"
 
-sh -c "sleep 10; echo Initializing the replicate; mongo --eval 'printjson(rs.initiate())'" &
-sh -c "sleep 20; echo Checking replicate state; mongo --eval 'printjson(rs.status())'" &
+sh -c "sleep 20; echo Initializing the replicate; mongo --eval 'printjson(rs.initiate())'" &
+sh -c "sleep 30; echo Checking replicate state; mongo --eval 'printjson(rs.status())'" &
 
 mongod --replSet ${replicate_name} --oplogSize ${oplog_size} --smallfiles
